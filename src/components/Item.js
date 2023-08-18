bf5ccdabe40ccdb9f9d134b0ebde1389f2b5b4b2
mark_kibo <kibochamark@gmail.com>
2023-08-18 15:56:54 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ name, category }) {
+function Item({ name, category , id}) {
+  const[cart, setCart]=useState([])
+  const[cartClass, setCartClass]=useState("")
+  function handeItem(e){
+   let data={
+    "id":id,
+    "name":name,
+    "category": category    
+   }
+   if(cart.map(obj => obj.id).includes(id)){
+    let newCart=cart.filter(obj => obj.id !== id)
+    setCart(newCart)
+    let newClass=""
+    setCartClass(newClass)
+   }else{
+    let newCart=[...cart, data]
+    setCart(newCart)
+    let newClass="in-cart"
+    setCartClass(newClass)
+   }
+  }
+
+  
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartClass} onClick={handeItem}>{cartClass === "in-cart"? "Add to Cart": "Remove From Cart"}</button>
     </li>
   );
 }
